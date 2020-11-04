11b64f24a9e8a5d54682c1901c69447beccae9b4
Iida Hämäläinen <iida.t.hamalainen@gmail.com>
2020-11-04 11:14:17 +0200

    viikko 2 tehtävät 9-12

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
@@ -0,0 +1,22 @@
+package ohtu.verkkokauppa;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Pankki implements PankkiRajapinta {
+
+    private KirjanpitoRajapinta kirjanpito;
+
+    public Pankki(KirjanpitoRajapinta kirjanpito) {
+        this.kirjanpito = kirjanpito;
+    }
+
+    @Override
+    public boolean tilisiirto(String nimi, int viitenumero, String tililta, String tilille, int summa) {
+        kirjanpito.lisaaTapahtuma("tilisiirto: tililtä " + tililta + " tilille " + tilille
+                + " viite " + viitenumero + " summa " + summa + "e");
+
+        // täällä olisi koodi joka ottaa yhteyden pankin verkkorajapintaan
+        return true;
+    }
+}
